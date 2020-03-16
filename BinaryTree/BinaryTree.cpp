b958b61fca6776f8ca85eb4fedb125d0c1accc68
DmitryLaptev <60900680+DmitryLaptev@users.noreply.github.com>
2020-03-16 23:17:40 +0300

    Add files via upload
    Добавлен изменённый шаблон бинарного дерева

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -5,99 +5,27 @@
 #include <vector>
 #include "BinaryTree.h"
 using namespace std;
-template <class T>
-BinaryTree<T>* Create(T val, BinaryTree<T>* l = NULL, BinaryTree<T>* r = NULL) {
-	BinaryTree<T>* tree;
-	tree = new BinaryTree<T>(val, l, r);
-	if (tree == NULL) {
-		cout << "Error" << endl;
-		return;
-	}
-	return tree;
-}
-template <class T>
-void Order(BinaryTree<T>* tree, void visit(T& val)) {
-	if (tree != NULL) {
-		Order(tree->Left(), visit);
-		visit(tree->value);
-		Order(tree->Right(), visit);
-	}
-}
-template <class T>
-BinaryTree<T>* SearchTree<T>::SearchNode(const T& val, BinaryTree<T>*& parent) const {
-	BinaryTree<T>* tree = root;
-	parent = NULL;
-	while (tree != NULL) {
-		if (val == tree->value) break;
-		else {
-			parent = tree;
-			if (val < tree->value) tree = tree->left;
-			else tree = tree->right;
-		}
-	}
-	return tree;
-}
-template <class T>
-int SearchTree<T>::Search(T& val) {
-	BinaryTree<T>* parent;
-	node = SearchNode(val, parent);
-	if (node != NULL) {
-		val = node->value;
-		return true;
-	}
-	else return false;
-}
-template <class T>
-void SearchTree<T>::Insert(const T& val) {
-	BinaryTree<T>* tree = root,parent = NULL,*Node;
-	while (tree != NULL) {
-		parent = tree;
-		if (val < tree->value) tree = tree->left;
-		else tree = tree->right;
-	}
-	if (parent == NULL) root = Node;
-	else if (val < parent->value) parent->left = Node;
-	else parent->right = Node;
-	node = Node;
-	count++;
-}
-template <class T>
-void FreeTree(BinaryTree<T>* tree) {
-	delete tree;
-}
-template <class T>
-void SearchTree<T>::Delete(const T& val) {
-	BinaryTree<T>* parent, * child1, * child2;
-	if ((child1 = SearchNode(val, parent)) == NULL) return;
-	if (child1->right == NULL) child2 = child1->left;
-	else if (child1->left == NULL) child2 = child1->right;
-	else {
-		BinaryTree<T>* parentOfSon = child1;
-		child2 = child1->left;
-		while (child2->right != NULL) {
-			parentOfSon = child2;
-			child2 = child2->right;
-		}
-		if (parentOfSon == child1) child2->right = child1->right;
-		else parentOfSon->right = child2->left;
-	}
-	if (child2 == NULL) root = child2;
-	else if (child1->value < parent->value) parent->left = child2;
-	else parent->right = child2;
-	FreeTree(child1);
-	count--;
-}
-template <class T>
-void leafList(SearchTree<T>* tree) {
-	if (tree->left == nullptr && tree->right == nullptr) cout << tree->value << endl;
-	else {
-		if (tree->left) leafList(tree->left);
-		if (tree->right) leafList(tree->right);
-	}
-}
+
+
 int main()
 {
-
+	BinaryTree<int> tree;
+	vector<int>v = tree.Order();
+	tree.Insert(7);
+	tree.Insert(11);
+	tree.Insert(15);
+	tree.Insert(2);
+	cout << (string)tree << endl;
+	tree.Delete(2);
+	tree.SearchNode(7);
+	tree.Delete(15);
+	cout << (string)tree << endl;
+	tree.Insert(9);
+	tree.Insert(17);
+	vector<int> leafList = tree.leafList();
+	for (int i = 0; i < leafList.size(); i++) cout << leafList[i] << endl;
+	vector<int> ord = tree.Order();
+	for (int i = 0; i < ord.size(); i++) cout << ord[i] << endl;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
